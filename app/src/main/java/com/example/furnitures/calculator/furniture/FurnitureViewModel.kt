7275d22e99cc57ac7fef60d0d84500ac16ddc58f
Vladimir Brazhnik <vb@fluidmobile.de>
2019-05-20 09:26:49 +0200

    Add LiveData for filter, data, mediator & viewState
    Filter with param FurnitureCategory
    Data with access to repository
    Mediator with data & filter LiveData
    ViewState with Mediator and mapping func

--- app/src/main/java/com/example/furnitures/calculator/furniture/FurnitureViewModel.kt
+++ app/src/main/java/com/example/furnitures/calculator/furniture/FurnitureViewModel.kt
@@ -1,11 +1,7 @@
 package com.example.furnitures.calculator.furniture
 
 import android.app.Application
-import android.arch.lifecycle.AndroidViewModel
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.Transformations
-import java.util.*
+import android.arch.lifecycle.*
 
 /**
  * Copyright (c) 2017 fluidmobile GmbH. All rights reserved.
@@ -14,13 +10,34 @@ class FurnitureViewModel(application: Application) : AndroidViewModel(applicatio
 
     private val repository = FurnitureRepositoryImp()
 
+    // Wenn sich die Daten im Repository ändern -> zugreifen auf die Filter und diese anwenden
+    // Wenn sich Filter ändern -> zugreifen auf die Daten und diese verändern
     // Daten Liste (Oberste Schicht)
     private val furnitures = repository.getFurnitures()
+    private val furnituresFilter = MutableLiveData<FurtnitureCategory>()
+    private val furnitureMediator = MediatorLiveData<List<Furniture>>()
+
     // Gemappte Daten für den View -> Observed alle Änderungen der Daten in Transformations.map
-    private val furnituresViewState = Transformations.map(furnitures, ::mapList)
+    private val furnituresViewState = Transformations.map(furnitureMediator, ::mapList)
 
     private val selectedFurnitures = repository.getSelectedFurnitures()
     private val selectedFurnituresViewState = Transformations.map(selectedFurnitures, ::mapList)
+
+    init {
+        furnitureMediator.addSource(furnitures) { furniture ->
+            val filter = furnituresFilter.value
+            if (filter == null) furnitureMediator.value = furniture
+            else furnitureMediator.value = filter(furniture!!, filter)
+
+        }
+        furnitureMediator.addSource(furnituresFilter) { filter ->
+            val furniture = furnitures.value
+            if (furniture != null) {
+                if (filter == null) furnitureMediator.value = furniture
+                else furnitureMediator.value = filter(furniture, filter)
+            }
+        }
+    }
 
     override fun onFurnitureClicked(furniture: FurnitureViewState) {
         repository.updateFurnitureOnClick(furniture)
