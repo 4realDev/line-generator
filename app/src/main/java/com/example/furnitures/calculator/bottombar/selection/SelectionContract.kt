0de2c25072a3babfe3eb61eaa8ab207479cf8ccc
Vladimir Brazhnik <vb@fluidmobile.de>
2019-06-24 11:58:59 +0200

    Clean up SelectionContract

--- app/src/main/java/com/example/furnitures/calculator/bottombar/selection/SelectionContract.kt
+++ app/src/main/java/com/example/furnitures/calculator/bottombar/selection/SelectionContract.kt
@@ -7,14 +7,9 @@ import com.example.furnitures.calculator.trick.RowViewState
 interface FurnitureContract {
 
     interface ViewModel {
-        //region Actions
+
         fun onFurnitureClicked(furniture: FurnitureViewState)
 
-//        fun onFilterFurnitures(filter: FurnitureCategory)
-//        //endregion
-//
-//        //region Data
-//        fun getFurnitureList(): LiveData<List<FurnitureViewState>>
         fun getFurnitureViewStateWithHeader(): LiveData<List<RowViewState>>
     }
 }
